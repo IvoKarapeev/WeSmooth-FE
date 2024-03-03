fca94e63d57d800872fef004a4d57c536a1b2ae8
Boris Georgiev <bgeorgiev@pros.com>
2024-03-04 01:05:32 +0200

    FE-4: Logout logic

--- we-smooth-fe/src/App.jsx
+++ we-smooth-fe/src/App.jsx
@@ -1,11 +1,21 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { useSharedSelector } from './store/shared/sharedStore'
 import { PocBlueprintsPage } from './pages/poc-blueprints-api/pocBlueprintsPage'
+import { setAccessToken, setUser } from './store/shared/sharedSlice'
+import { useSharedDispatch } from './store/shared/sharedStore'
 
 function App() {
     const { logout } = useAuth0()
     const user = useSharedSelector((state) => state.shared.user)
     const accessToken = useSharedSelector((state) => state.shared.accessToken)
+
+    const sharedDispatch = useSharedDispatch()
+
+    const handleLogout = () => {
+        sharedDispatch(setUser(undefined))
+        sharedDispatch(setAccessToken(undefined))
+        logout()
+    }
 
     return (
         <div className="App">
@@ -13,7 +23,7 @@ function App() {
                 <>
                     <p>Hello {user.name}! Welcome to WeSmooth!</p>
                     <p>Your access token is: {accessToken}</p>
-                    <button onClick={logout}>Logout</button>
+                    <button onClick={handleLogout}>Logout</button>
                     <PocBlueprintsPage />
                 </>
             )}
